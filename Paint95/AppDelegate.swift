d9ce4e44a9bec969413843f8ec98d47aeb9fb7a5
jjmcinto@gmail.com <jjmcinto@gmail.com>
2025-07-10 13:54:55 +0700

    Initial Commit

---
+++ Paint95/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  Paint95
+//
+//  Created by Van Do on 7/10/25.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ Paint95Tests/Paint95Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Paint95Tests.swift
+//  Paint95Tests
+//
+//  Created by Van Do on 7/10/25.
+//
+
+import Testing
+@testable import Paint95
+
+struct Paint95Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
